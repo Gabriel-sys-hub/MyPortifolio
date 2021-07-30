0fdd6c71025a9fcfffd7c87781b6b4125cc8a82b
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-07-30 11:57:42 -0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './components/Home/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import About from './components/About/About';
 import NavBar from './components/NavBar/NavBar';
 import Skills from './components/Skills/Skills';
@@ -9,18 +9,18 @@ import Projects from './components/Projects/Projects';
 
 function App() {
   return (
-    <Router basename="/MyPortifolio">
-    <div className="App">
-      <NavBar />
-      <Switch>
-      <Route exact path="/" component={ Home } />
-      <Route exact path="/About" component={ About } />
-      <Route exact path="/skills" component={ Skills } />
-      <Route exact path="/contact" component={ Contact } />
-      <Route exact path="/Projects" component={ Projects } />
+    <BrowserRouter basename="/MyPortifolio">
+      <div className="App">
+        <NavBar />
+        <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/About" component={ About } />
+        <Route exact path="/skills" component={ Skills } />
+        <Route exact path="/contact" component={ Contact } />
+        <Route exact path="/Projects" component={ Projects } />
       </Switch>
-    </div>
-    </Router>
+      </div>
+    </BrowserRouter>
   );
 }
 
